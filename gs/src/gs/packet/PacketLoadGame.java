3938f4483ea59e8156ed7cdd068de2962daaa874
Daewoo, Kim <bighouse.kim@gmail.com>
2014-12-06 08:50:12 +0900

    - MySQL 테스트 코드 추가.

--- gs/src/gs/packet/PacketLoadGame.java
+++ gs/src/gs/packet/PacketLoadGame.java
@@ -11,13 +11,16 @@ import org.slf4j.LoggerFactory;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.msgpack.annotation.Message;
 
 import common.model.Game;
 import common.model.Mail;
-import common.model.Share;
 import common.model.Stage;
 import common.storage.AppCouchbaseImpl;
+import common.storage.AppMySQLImpl;
 
 public class PacketLoadGame extends PacketBase {
 	@Message
@@ -50,9 +53,13 @@ public class PacketLoadGame extends PacketBase {
 	public PacketLoadGame(LoadGameRequest request) {
 		this.request = request;
 	}
-	
+
 	@Override
 	public boolean processRequest() {
+		return processRequestMySQL();
+	}
+
+	public boolean processRequestCouchbase() {
 		Game game = AppCouchbaseImpl.get().getGame(request.channelId);
 		
 		if (game == null) {
@@ -75,6 +82,69 @@ public class PacketLoadGame extends PacketBase {
 		} else {
 		}
 		
+		response.game = game;
+
+		response.result = RESULT_LOAD_GAME_OK;
+		response.now = System.currentTimeMillis();
+		
+		return true;
+	}
+	
+	public boolean processRequestMySQL() {
+		Game game = null;
+		
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = AppMySQLImpl.get().getSessionFactory(0).getCurrentSession();
+			
+			if (session == null) {
+				return false;
+			}
+			
+			tx = session.getTransaction();
+			
+			if (tx == null) {
+				return false;
+			}
+
+			session.beginTransaction();
+			
+			game = (Game)session.createCriteria(Game.class).add(Restrictions.eq("channelId", request.channelId)).uniqueResult();
+			
+			if (game == null) {
+				game = new Game();
+				game.channelId = request.channelId;
+				game.nickName = request.nickName;
+				game.marketId = request.marketId;
+				game.pushId = request.pushId;
+				game.selStageId = 0;
+				game.loadDate = new Date(now);
+				game.createDate = new Date(now);
+				
+				Stage stage = new Stage();
+				stage.game = game;
+				stage.stageId = 1;
+				stage.topScore = 0;
+				
+				game.stages.put((long)stage.stageId, stage);
+				
+				session.save(game);
+			} else {
+			}
+			
+			tx.commit();
+		} catch (Exception e) {
+			if (session != null && tx != null)
+				tx.rollback();
+			
+			return false;
+		} finally {
+			if (session != null && session.isOpen())
+				session.close();
+		}
+
 		response.game = game;
 
 		response.result = RESULT_LOAD_GAME_OK;
